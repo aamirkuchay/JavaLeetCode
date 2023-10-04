b911a1784b36822226417ae40216eaa56a3ea8cf
Aamir Nazir Kuchay <aamir1kuchay@gmail.com>
2023-10-04 15:44:44 +0530

    single number

--- src/main/java/org/example/All/Main.java
+++ src/main/java/org/example/All/Main.java
@@ -5,35 +5,27 @@ import java.util.*;
 
 public class Main {
 
-public static int[] findTopK(int[] nums, int k){
-    PriorityQueue<Integer> heap = new PriorityQueue<>();
-
-    for(int i=0;i<k;i++){
-        heap.add(i);
-    }
-
-
-    for(int i=0;i<nums.length;i++){
-        if(nums[i] > heap.peek()){
-            heap.poll();
-            heap.add(nums[i]);
-        }
-    }
-
-    int[] r = new int[k];
-    for(int i=0;i<k;i++){
-        r[i] = heap.poll();
-    }
-    return r;
-}
-
     public static void main(String[] args) {
-        int[] nums = {22,32,45,56,43,68,90,65,22,46,78};
-        int k =3;
-        int[] r= findTopK(nums,k);
-        for(int i:r){
-            System.out.println(i);
+
+        int[] arr = {6,23,45,23,57,78,77,32};
+
+        int large = Integer.MIN_VALUE;
+        int second = Integer.MIN_VALUE;
+        int small = Integer.MAX_VALUE;
+
+        for(int i=0;i<arr.length;i++){
+            if(arr[i] > large){
+                second = large;
+                large = arr[i];
+            } else if (arr[i] > second && arr[i] < large) {
+                second = arr[i];
+            }
+
         }
+        System.out.println(large);
+        System.out.println(second);
+
+
 
     }}
 
