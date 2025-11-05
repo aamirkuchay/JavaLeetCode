61e03133a92eac3c5824a514a4f10fcf90b50cdd
Aamir Kuchay <aamir1kuchay@gmail.com>
2025-11-05 14:56:14 +0530

    xsdx

--- src/main/java/org/example/Practise.java
+++ src/main/java/org/example/Practise.java
@@ -8,22 +8,6 @@ import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class Practise {
-
-    public static int[] twoSum(int[] num , int target){
-        HashMap<Integer,Integer> maps = new HashMap<>();
-        for(int i=0;i<num.length;i++){
-            int compliment = target - num[i];
-
-            // Check if the complement exists in the map
-            if(maps.containsKey(compliment)){
-                return new int[]{maps.get(compliment), i};
-            }
-            // Otherwise, store the current number and its index
-            maps.put(num[i],i);
-        }
-        return new int[]{};
-    }
-
     public static void main(String[] args) {
         int[] nums = {2,7,11,15};
         int target = 17;
